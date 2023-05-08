24ac450fbd14cab4dab02b4ae80860b420e7c6f1
Neil Vela <neilerickson512@gmail.com>
2023-05-08 01:13:22 -0500

    avance logeo

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace Web_MSI.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Recordar Usuario")]
             public bool RememberMe { get; set; }
         }
 
