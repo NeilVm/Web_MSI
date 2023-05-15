a770a5f01d4e5a62e73b84d26a50273afed592df
Neil Vela <neilerickson512@gmail.com>
2023-05-14 20:46:41 -0500

    tabla Reporte Diario

--- Models/HojaRegistroD.cs
+++ Models/HojaRegistroD.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Web_MSI.Models;
 
 namespace Web_MSI.Models
 {
